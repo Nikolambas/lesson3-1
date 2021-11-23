03d172e1a9189990524dcccb19253b5d61e77afc
andrianov_ng <andrianov93@inbox.ru>
2021-11-23 18:32:41 +0300

    Initial commit

---
+++ Apple.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Apple extends Fruit {
+
+    private final String name = "Apple";
+    private final Float weight = 1.0f;
+
+    public Float getWeight() {
+        return weight;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ Fruit.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Fruit {
+    private  String name;
+    private Float weight;
+
+    public Float getWeight() {
+        return weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
