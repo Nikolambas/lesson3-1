c3939119fafe4a8b33a53fa1ae1e80fb2f0827fd
Nikolambas <88347156+Nikolambas@users.noreply.github.com>
2021-11-23 20:43:24 +0300

    Update Orange.java

--- Orange.java
+++ Orange.java
@@ -1,15 +1,6 @@
 package com.company;
 
-public class Orange extends Fruit{
-    private final String name = "Orange";
-    private final Float weight = 1.5f;
-
-    public Float getWeight() {
-        return weight;
-    }
-
-    public String getName() {
-        return name;
-    }
+public class Orange {
+    
 
 }
