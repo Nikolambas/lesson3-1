6549b142872d63de38bc74e1e78237d714307dd4
Nikolambas <88347156+Nikolambas@users.noreply.github.com>
2021-11-23 20:42:46 +0300

    Update Fruit.java

--- Fruit.java
+++ Fruit.java
@@ -1,14 +1,5 @@
 package com.company;
 
 public class Fruit {
-    private  String name;
-    private Float weight;
-
-    public Float getWeight() {
-        return weight;
-    }
-
-    public String getName() {
-        return name;
-    }
+    
 }
